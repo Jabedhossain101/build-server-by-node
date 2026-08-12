9b8e269b771fd4d12e524131be05d654f9848201
rafsan102 <ahmedrafsan101@gmail.com>
2026-08-13 04:06:33 +0600

    create basic server with get operation

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+// import http, { IncomingMessage, Server, ServerResponse } from 'http';
+// import path from 'path';
+
+// const server: Server = http.createServer((req:IncomingMessage, res:ServerResponse) => {
+//   console.log('server is running...');
+//   if (req.url == '/' && req.method == 'GET') {
+//     res.writeHead(200, { 'content-type': 'application/json' });
+//     res.end(
+//       JSON.stringify({
+//         message: 'Hello from node js with typescript...',
+//         path: req.url,
+//       })
+
+//     )
+// }
+// })
+// server.listen(5000, () => {
+//   console.log(`Server is running on port ${5000}`);
+// })
+
+import http, { IncomingMessage, Server, ServerResponse } from 'http'
+import path from 'path'
+
+
+const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  console.log('server is running by node....');
+  if (req.url == '/', req.method = 'GET') {
+    res.writeHead(200, { 'content-type': 'application/json' })
+    res.end(
+      JSON.stringify(
+        {
+          message: 'this is run by port',
+          path: req.url,
+        }
+      )
+    )
+  }
+})
+
+server.listen(5000, () => {
+  console.log(`server is running on ${5000}`);
+})
