9bcea6752cb1c7d9552dc4e6f6060d8443c201c2
rafsan102 <ahmedrafsan101@gmail.com>
2026-08-14 01:07:32 +0600

    create post method on server and test

--- src/server.ts
+++ src/server.ts
@@ -18,24 +18,67 @@
 //   console.log(`Server is running on port ${5000}`);
 // })
 
-import http, { IncomingMessage, Server, ServerResponse } from 'http'
-import path from 'path'
-import config from './config'
+import http, { IncomingMessage, Server, ServerResponse } from 'http';
+import path from 'path';
+import config from './config';
 
-const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-  
-  if (req.url == '/' && req.method == 'GET') {
-    res.writeHead(200,{'content-type': 'application/json'})
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    //-------root route----------
+    if (req.url == '/' && req.method == 'GET') {
+      res.writeHead(200, { 'content-type': 'application/json' });
 
-    res.end(
-      JSON.stringify({
-        message: 'hello i am node js',
-        path:req.url
-      })
-    )
-  } 
-})
+      res.end(
+        JSON.stringify({
+          message: 'hello i am node js',
+          path: req.url,
+        }),
+      );
+    }
+    //------------health route--------
+    if (req.url == '/api' && req.method == 'GET') {
+      res.writeHead(200, { 'content-type': 'application/json' });
+
+      res.end(
+        JSON.stringify({
+          message: 'heath status okay',
+          path: req.url,
+        }),
+      );
+    }
+
+
+    // -------User data post method------
+    
+    if (req.url == '/api/users' && req.method == 'POST') {
+      // const user = {
+      //   id: 1,
+      //   name: 'alice',
+      // };
+      // res.writeHead(200, { 'content-type': 'application/json' });
+      // res.end(JSON.stringify(user));
+
+      let body = ' ';
+      //listen for data chunk
+
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+
+      req.on('end', () => {
+        try {
+          const parseBody = JSON.parse(body);
+          console.log(parseBody);
+console.log('catching on change');
+          res.end(JSON.stringify(parseBody));
+        } catch (err:any) {
+          console.log(err?.message);
+        }
+      });
+    }
+  },
+);
 
 server.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
-})
+});
